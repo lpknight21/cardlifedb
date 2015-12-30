f909ac5d96e52c86921308db0fa7eb80ac9713e0
Brian Hooper <brian.hooper@admins-MacBook-Pro-2.local>
2015-12-30 15:38:45 -0600

    Latest Build and Firebase Deployment

--- .tmp/concat/scripts/scripts.js
+++ .tmp/concat/scripts/scripts.js
@@ -48,6 +48,50 @@ var app = angular.module('starterlog', ['firebase','ui.bootstrap','ui.router', '
           }]
         }
       })
+      // BLOG
+      .state('blog-posts', {
+        url: '/blog/posts',
+        controller: 'PostsCtrl as postsCtrl',
+        templateUrl: 'blog/index.html',
+        resolve: {
+          alarms: ["Posts", function (Posts){
+             return Posts();
+           }],
+          auth: ["$state", "Users", "Auth", function($state, Users, Auth){
+            return Auth.$requireAuth().catch(function(){
+              $state.go('home');
+            });
+          }]
+        }
+      })
+      .state('blog-post-create', {
+        url: '/blog/posts/create',
+        templateUrl: 'blog/create.html',
+        controller: 'PostsCtrl as postsCtrl',
+        resolve: {
+          auth: ["$state", "Users", "Auth", function($state, Users, Auth){
+            return Auth.$requireAuth().catch(function(){
+              $state.go('home');
+            });
+          }]
+        }
+      })
+      .state('blog-post-view', {
+        url: '/blog/posts/view/{postId}',
+        templateUrl: 'blog/view.html',
+        controller: 'PostsCtrl as postsCtrl'
+      })
+      .state('blog-post-preview', {
+        url: '/blog/posts/preview/{postId}',
+        templateUrl: 'blog/preview.html',
+        controller: 'PostsCtrl as postsCtrl'
+      })
+      .state('posts/edit', {
+        url: '/blog/posts/edit/{postId}',
+        templateUrl: 'blog/edit.html',
+        controller: 'PostsCtrl as postsCtrl'
+      })
+      // Pages Content
       .state('pages-about', {
         url: '/pages/about',
         templateUrl: 'pages/about.html'
@@ -247,3 +291,110 @@ app.controller('ProfileCtrl', ["$state", "Auth", "auth", "profile", function($st
   }]);
 
 console.log('--> starterlog/app/users.profile.js loaded');
+
+'use strict';
+
+app.factory("Posts", ["FIREBASE_URL", "$firebaseArray", function PostFactory(FIREBASE_URL, $firebaseArray) {
+  return function(){
+    // snapshot of our data
+    var ref = new Firebase(FIREBASE_URL + 'posts');
+    // returning synchronized array
+    return $firebaseArray(ref);
+  }
+}]);
+
+console.log('--> starterlog/app/blog/posts.service.js loaded');
+
+'use strict';
+
+app.controller("PostsCtrl", ["$state", "$scope", "FIREBASE_URL", "$firebaseObject", "$firebaseArray", "$stateParams", "ngTableParams", "$filter", "Posts", function($state, $scope, FIREBASE_URL, $firebaseObject, $firebaseArray, $stateParams, ngTableParams, $filter, Posts) {
+
+    $scope.posts = Posts();
+
+    // CORE CRUD FUNCTIONALITY
+    // - CREATE ($add firebaseObject to synchronized firebaseArray)
+    // - READ (get firebaseObject using stateParams and Firebase Reference)
+    // - UPDATE ($save firebaseObject)
+    // - DELETE ($remove firebaseObject)
+
+    // CREATE - ADD A NEW POST TO FIREBASE
+    $scope.create = function(post) {
+      $scope.posts.$add(post).then(function() {
+        console.log('Blog Post Created');
+
+        //$location.path('/posts');
+        $state.go('blog-posts');
+
+      }).catch(function(error) {
+        console.log(error);
+      });
+    };
+
+    // READ - GET A POST FROM FIREBASE ON PAGE INIT FOR /posts/edit/:id route
+    $scope.getPost = function() {
+      var ref = new Firebase(FIREBASE_URL + 'posts');
+      $scope.post = $firebaseObject(ref.child($stateParams.postId));
+    };
+
+    // UPDATE - EDIT A POST AND SAVE IT TO FIREBASE
+    $scope.update = function() {
+      // save firebaseObject
+      $scope.post.$save().then(function(){
+        console.log('Blog Post Updated');
+
+        // redirect to /posts path after update
+        $state.go('blog-posts');
+      }).catch(function(error){
+        console.log(error);
+      });
+    };
+
+    // DELETE - REMOVE A POST FROM FIREBASE
+    $scope.delete = function(post) {
+        $scope.posts.$remove(post).then(function(){
+            console.log('Post Deleted');
+
+            // redirect to /posts path after delete
+            $state.go('blog-posts');
+        }).catch(function(error){
+            console.log(error);
+        });
+    };
+
+
+    // DATA TABLE SYNCHRONIZATION USING NG-TABLE
+
+    // Since the data is asynchronous we'll need to use the $loaded promise.
+    // Once data is available we'll set the data variable and init the ngTable
+    $scope.posts.$loaded().then(function(posts) {
+      console.log(posts.length); // data is loaded here
+      var data = posts;
+
+      $scope.tablePosts = new ngTableParams({
+            page: 1,            // show first page
+            count: 10,          // count per page
+            sorting: { title: 'asc' }    // initial sorting
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                // use build-in angular filter
+                var orderedData = params.sorting() ? $filter('filter')(data, params.filter()) : data;
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
+    });
+
+    // Listening for list updates to Posts to update DataTable
+    var ref = new Firebase(FIREBASE_URL + 'posts');
+    var list = $firebaseArray(ref);
+    list.$watch(function(event) {
+      console.log(event);
+      $scope.posts.$loaded().then(function(){
+        $scope.tablePosts.reload();
+      });
+    });
+
+}]);
+
+console.log('--> starterlog/app/blog/posts.controller.js loaded');
