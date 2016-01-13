be1a0aa3417a555878e866f3e08f526d5a0ca3c6
Brian Hooper <brian.hooper@edointeractive.com>
2016-01-13 10:56:45 -0600

    Working to resolve imagemin build error Cannot Read property contents of undefined

--- Gruntfile.js
+++ Gruntfile.js
@@ -275,7 +275,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '{,*/}*.png',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
